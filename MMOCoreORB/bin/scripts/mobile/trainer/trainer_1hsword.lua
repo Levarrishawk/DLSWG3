b7692048975a49935780e5f371604d179ee80db8
Levarrishawk <levarrishawk@gmail.com>
2015-12-16 03:57:35 -0500

    jedi trainer fix?

--- MMOCoreORB/bin/scripts/mobile/trainer/trainer_1hsword.lua
+++ MMOCoreORB/bin/scripts/mobile/trainer/trainer_1hsword.lua
@@ -1,6 +1,7 @@
 trainer_1hsword = Creature:new {
-	objectName = "@mob/creature_names:trainer_1hsword",
-	randomNameType = NAME_GENERIC_TAG,
+	objectName = "",
+	customName = "a Jedi trainer",
+	--randomNameType = NAME_GENERIC_TAG,
 	socialGroup = "",
 	faction = "",
 	level = 100,
