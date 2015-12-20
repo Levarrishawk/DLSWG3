46c6001fad00fe74c2a99de3f6c2bd7c041f487f
Levarrishawk <levarrishawk@gmail.com>
2015-12-20 07:10:01 -0500

    activateRebelHideout

--- MMOCoreORB/bin/scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -312,7 +312,7 @@ includeFile("cities/yavin4_mining_outpost.lua")
 
 -- POIs
 --includeFile("poi/corellia_rogue_corsec_base.lua")
---includeFile("poi/corellia_rebel_hideout.lua")
+includeFile("poi/corellia_rebel_hideout.lua")
 includeFile("poi/corellia_stronghold.lua")
 includeFile("poi/dantooine_abandoned_rebel_base.lua")
 includeFile("poi/dantooine_dantari_village.lua")
