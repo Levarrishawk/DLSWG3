6c5ce8146787800532519c0a3db3e9ca1485bcf4
Levarrishawk <levarrishawk@gmail.com>
2016-01-11 22:09:45 -0500

    step1

--- MMOCoreORB/src/server/zone/objects/creature/commands/TendDamageCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/TendDamageCommand.h
@@ -7,6 +7,7 @@
 
 #include "server/zone/objects/scene/SceneObject.h"
 #include "TendCommand.h"
+#include "server/zone/objects/creature/events/InjuryTreatmentTask.h"
 
 class TendDamageCommand : public TendCommand {
 public:
