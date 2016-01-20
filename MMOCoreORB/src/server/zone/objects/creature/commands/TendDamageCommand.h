7e3620110ed80a242dacf63b9039513143a13b98
Skyyyr <skydeath227@yahoo.com>
2016-01-19 16:57:53 -0800

    Update TendDamageCommand.h
    test mode.

--- MMOCoreORB/src/server/zone/objects/creature/commands/TendDamageCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/TendDamageCommand.h
@@ -14,18 +14,32 @@ public:
 
 	TendDamageCommand(const String& name, ZoneProcessServer* server)
 			: TendCommand(name, server) {
-		effectName = "clienteffect/medic_heal.cef";
-
-		actionCost = 900;
-		mindWoundCost = 0;
-
-		tendDamage = true;
-
-		healthHealed = 1250;
-		//actionHealed = 50;
-
-		//defaultTime = 5.0;
-		range = 0;
+		/*
+		TODO:
+			Attach buff
+			Duration (cooldown)
+			Check for 'if on cooldown'
+		*/
+		
+		
+		uint32 buffcrc = BuffCRC::JEDI_RESIST_BLEEDING;
+		StringIdChatParameter startStringId("medical_heal", "apply_healCooldown");
+		StringIdChatParameter endStringId("medical_heal", "remove_healCooldown");
+		duration = 10;
+		ManagedReference<Buff*> buff = new Buff(creature, buffcrc, duration, BuffType::JEDI);
+		Locker locker(buff);
+		buff->setStartMessage(startStringId);
+		buff->setEndMessage(endStringId);
+		
+		if (!creature->hasbuff(BuffCRC:JEDI_RESIST_BLEEDING)) {
+			creature->addBuff(buff);
+			tendDamage = true;
+			healthHealed = 1250;
+			actionCost = 900;
+			creature->playEffect("clienteffect/medic_heal.cef", "");
+		} else {
+			creature->sendSystemMessage("You're Still on cooldown");	
+		}
 	}
 
 };
