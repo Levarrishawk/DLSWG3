bbed8ee59fb3dd366c7107b3ca452bf4912ea8e9
Skyyyr <skydeath227@yahoo.com>
2016-01-24 18:54:09 -0800

    Update setNormalTask.h

--- MMOCoreORB/src/server/zone/objects/player/events/setNormalTask.h
+++ MMOCoreORB/src/server/zone/objects/player/events/setNormalTask.h
@@ -40,7 +40,7 @@ public:
 		try {
 			Reference<setNormalTask*> snormalTask = player->getPendingTask("resetspeed").castTo<setNormalTask*>();
 
-			player->setSpeedMultiplierBase(1f, true);
+			player->setSpeedMultiplierBase(1.0f, true);
 			player->playEffect("clienteffect/pl_force_resist_bleeding_self.cef", "");
 
 		} catch (Exception& e) {
