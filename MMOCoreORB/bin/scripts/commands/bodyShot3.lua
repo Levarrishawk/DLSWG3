f36867a5199e774b23a49dd1d90c4fe23514bd50
Levarrishawk <levarrishawk@gmail.com>
2016-01-30 03:31:56 -0500

    Strike Coordinator: Balance Pass 2

--- MMOCoreORB/bin/scripts/commands/bodyShot3.lua
+++ MMOCoreORB/bin/scripts/commands/bodyShot3.lua
@@ -44,7 +44,7 @@
 BodyShot3Command = {
         name = "bodyshot3",
 
-	damageMultiplier = 5,
+	damageMultiplier = 2.5,
   speedMultiplier = 1,
   healthCostMultiplier = 0,
   actionCostMultiplier = 1.25,
