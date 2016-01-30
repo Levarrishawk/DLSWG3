968345ab2a9d68ecc96ea419fc23ac53119be2d7
Levarrishawk <levarrishawk@gmail.com>
2016-01-30 00:18:19 -0500

    ElectrocutionTrooper: Action Cost Finalization

--- MMOCoreORB/bin/scripts/commands/fireLightningCone2.lua
+++ MMOCoreORB/bin/scripts/commands/fireLightningCone2.lua
@@ -47,7 +47,7 @@ FireLightningCone2Command = {
 	damageMultiplier = 1.5,
   speedMultiplier = 1,
   healthCostMultiplier = 0,
-  actionCostMultiplier = 2,
+  actionCostMultiplier = 1.75,
   mindCostMultiplier = 0,
 
   animationCRC = hashCode("fire_area_medium"),
